0558668ea4527e77c655cd11722cda6b442bbae8
Jakob Schäfer <mail@jakobschaefer.de>
2022-12-15 11:29:39 +0100

    wip: impl clap stub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+/// Verzion is a CLI tool that helps with versioning and changelog generation in CI/CD pipelines.
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Current,
+    Next,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Current => println!("0.0.0"),
+        Command::Next => println!("0.1.0"),
+    }
 }
